09710683402b8907dd07624c3c621079b22c67b5
vendi <svitlana.vakulenko@gmail.com>
2017-11-28 22:17:17 +0100

    new: Twitter API settings place holder

---
+++ twitter_settings_changeme!.py
@@ -0,0 +1,10 @@
+#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+APP_KEY = 'CHANGEME'
+APP_SECRET = 'CHANGEME'
+OAUTH_TOKEN = 'CHANGEME'
+OAUTH_TOKEN_SECRET = 'CHANGEME'
+
+# Twitter user name
+MY_NAME = 'CHANGEME'
